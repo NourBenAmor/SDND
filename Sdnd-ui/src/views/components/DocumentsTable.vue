bbf89d093989d136f19066554378641fb4476f3f
jasser21 <jesuisjasser@gmail.com>
2024-03-12 10:33:13 +0100

    commit

--- Sdnd-ui/src/views/components/DocumentsTable.vue
+++ Sdnd-ui/src/views/components/DocumentsTable.vue
@@ -93,21 +93,22 @@ const documents = ref([]);
 const filterText = ref('');
 const showModal = ref(false);
 const documentIndexToDelete = ref(null);
-const documents = ref([
-  {
-    title: 'John Michael',
-    type: 'PDF',
-    status: 'Online',
-    creationDate: '23/04/18',
-  },
-  {
-    title: 'Jane Doe',
-    type: 'JPG',
-    status: 'Offline',
-    creationDate: '25/04/18',
-  }
-]);
 
+const fetchDocuments = async () => {
+  try {
+    const response = await axios.get('https://localhost:7278/api/Document');
+    documents.value = response.data;
+  } catch (error) {
+    console.error('Error fetching documents:', error);
+  }
+};
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString();
+};
+onMounted(() => {
+  fetchDocuments();
+});
 
 const router = useRouter();
 
@@ -136,88 +137,3 @@ const hideModal = () => {
   showModal.value = false;
 };
 </script>
-<template>
-  <div class="documents-table-container m-1 p-5">
-
-    <div class="card px-3">
-      <div class="card-header d-flex justify-content-between align-items-center">
-        <h6 class="mb-3 flex-grow-3">Documents Table</h6>
-        <div class="d-flex flex-grow-2">
-          <input type="text" v-model="filterText" class="form-control m-3" placeholder="Search By...">
-          <a class="btn btn-link text-green "  @click="openAddDocumentView(document)" href="javascript:;">
-            <i class="fas fa-add text-green ms-2" aria-hidden="true"></i>
-          </a>
-        </div>
-      </div>
-      <div class="card-body px-0 pt-0 pb-2">
-        <div class="table-responsive p-0">
-          <table class="table align-items-center mb-0">
-            <thead>
-              <tr>
-                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                  Title
-                </th>
-                <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">
-                  Type
-                </th>
-                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                  Status
-                </th>
-                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                  Creation Date
-                </th>
-                <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(document, index) in documents" :key="index">
-                <td>
-                  <div class="d-flex px-2 py-1">
-                    <div class="d-flex flex-column justify-content-center">
-                      <h6 class="mb-0 text-sm">{{ document.title }}</h6>
-                    </div>
-                  </div>
-                </td>
-
-                <td>
-                  <p class="text-xs font-weight-bold mb-0">{{ document.type }}</p>
-                </td>
-                <td class="align-middle text-center text-sm">
-                  <span class="badge badge- bg-gradient-warning">{{ document.status }}</span>
-                </td>
-                <td class="align-middle text-center">
-                  <span class="text-secondary text-xs font-weight-bold">{{ document.creationDate }}</span>
-                </td>
-                <td class="align-middle">
-                  <div class="ms-auto text-end">
-                    <a class="btn btn-link text-green px-3 mb-0" @click="openDocumentView(document)"
-                      href="javascript:;">
-                      <i class="fas fa-eye text-green ms-2" aria-hidden="true"></i> {{ "  " }}View
-                    </a>
-                    <a class="btn btn-link text-dark px-3 mb-0" @click="openEditView(document)">
-                      <i class="fas fa-pencil-alt text-dark me-2" aria-hidden="true"></i>Edit
-                    </a>
-                    <a class="btn btn-link text-danger text-gradient px-3 mb-0" @click="showConfirmModal(index)"
-                      href="javascript:;">
-                      <i class="far fa-trash-alt me-2" aria-hidden="true"></i>Delete
-                    </a>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-
-        </div>
-      </div>
-    </div>
-
-  
-
-    <!-- Confirmation modal -->
-    <ConfirmationModalVue :show="showModal" message="Are you sure you want to delete this document?"
-      @confirm="handleConfirm" @cancel="hideModal" />
-  </div>
-</template>
-
