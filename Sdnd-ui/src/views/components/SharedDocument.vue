783958ba046a7d5c936272af05819365c19de6e6
Ayahadacha <161600848+Ayahadacha@users.noreply.github.com>
2024-03-25 01:30:39 +0100

    Create SharedDocument.vue

---
+++ Sdnd-ui/src/views/components/SharedDocument.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+      <h2>Documents Partagés</h2>
+      <ul>
+        <li v-for="document in sharedDocuments" :key="document.id">
+          <div>{{ document.title }}</div>
+          <div>{{ document.content }}</div>
+          <!-- Autres détails du document à afficher -->
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        sharedDocuments: []
+      };
+    },
+    mounted() {
+      this.fetchSharedDocuments();
+    },
+    methods: {
+      fetchSharedDocuments() {
+        const userId = 
+        axios.get(`/api/shareddocuments/${userId}`)
+          .then(response => {
+            this.sharedDocuments = response.data;
+          })
+          .catch(error => {
+            console.error('Erreur lors de la récupération des documents partagés', error);
+          });
+      }
+    }
+  };
+  </script>
+  
