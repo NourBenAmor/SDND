9e039fe7f1a2a5ba7d9054286e516d72e20d3738
wafa <njwafa40@gmail.com>
2024-03-29 23:33:45 +0100

    Mobile interface 2, camera scan, generate PDF document, save PDF

---
+++ sdnd-mobile/lib/page/compte/signup_page.dart
@@ -0,0 +1,163 @@
+
+import 'dart:io';
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'package:airsafe/page/commun/page_heading.dart';
+import 'package:airsafe/page/login_page.dart';
+import 'package:airsafe/page/commun/custom_form_button.dart';
+import 'package:airsafe/page/commun/custom_input_field.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _signupFormKey = GlobalKey<FormState>();
+
+  TextEditingController _nameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: const Color(0xffEEF1F3),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _signupFormKey,
+            child: Column(
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                    BorderRadius.vertical(top: Radius.circular(20)),
+                  ),
+                  child: Column(
+                    children: [
+                      const PageHeading(title: 'Sign-up',),
+                      const SizedBox(height: 16,),
+                      CustomInputField(
+                        labelText: 'Name',
+                        hintText: 'Your name',
+                        isDense: true,
+                        controller: _nameController,
+                        validator: (textValue) {
+                          if (textValue == null || textValue.isEmpty) {
+                            return 'Name field is required!';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16,),
+                      CustomInputField(
+                        labelText: 'Email',
+                        hintText: 'Your email id',
+                        isDense: true,
+                        controller: _emailController,
+                        validator: (textValue) {
+                          if (textValue == null || textValue.isEmpty) {
+                            return 'Email is required!';
+                          }
+                          if (!EmailValidator.validate(textValue)) {
+                            return 'Please enter a valid email';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 16,),
+                      CustomInputField(
+                        labelText: 'Password',
+                        hintText: 'Your password',
+                        isDense: true,
+                        obscureText: true,
+                        controller: _passwordController,
+                        validator: (textValue) {
+                          if (textValue == null || textValue.isEmpty) {
+                            return 'Password is required!';
+                          }
+                          return null;
+                        },
+                        suffixIcon: true,
+                      ),
+                      const SizedBox(height: 22,),
+                      CustomFormButton(innerText: 'Signup', onPressed: _handleSignupUser,),
+                      const SizedBox(height: 18,),
+                      SizedBox(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            const Text('Already have an account ? ', style: TextStyle(fontSize: 13, color: Color(0xff939393), fontWeight: FontWeight.bold),),
+                            GestureDetector(
+                              onTap: () => {
+                                Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()))
+                              },
+                              child: const Text('Log-in', style: TextStyle(fontSize: 15, color: Color(0xff748288), fontWeight: FontWeight.bold),),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 30,),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _handleSignupUser() async {
+    if (_signupFormKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Submitting data..')),
+      );
+
+      final userData = {
+        'name': _nameController.text,
+        'email': _emailController.text,
+        'password': _passwordController.text,
+      };
+
+      final jsonData = jsonEncode(userData);
+
+      try {
+        final response = await http.post(
+          Uri.parse('https://10.0.2.2:5261/api/Account/register'),
+          headers: <String, String>{
+            'Content-Type': 'application/json; charset=UTF-8',
+          },
+          body: jsonData,
+        );
+
+        if (response.statusCode == 200) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text('Registration successful!')),
+          );
+          // Vous pouvez naviguer vers une autre page après une inscription réussie
+          // Navigator.push(context, MaterialPageRoute(builder: (context) => NextPage()));
+        } else {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text('Registration failed: ${response.body}')),
+          );
+        }
+      } catch (error) {
+        print('Error during registration: $error');
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error during registration: $error')),
+        );
+      }
+    }
+  }
+}
