13dd35a173e979dfa665b5371b7d3b568b7cc276
jasser21 <jesuisjasser@gmail.com>
2024-04-16 10:11:57 +0100

    Program.cs edit

--- Sdnd-api/Program.cs
+++ Sdnd-api/Program.cs
@@ -12,10 +12,9 @@ using Sdnd_api.Services;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlite((builder.Configuration.GetConnectionString("SqliteConnection")));
-    //options.UseSqlServer((builder.Configuration.GetConnectionString("DefaultConnection")));
+    //options.UseSqlite((builder.Configuration.GetConnectionString("SqliteConnection")));
+    options.UseSqlServer((builder.Configuration.GetConnectionString("DefaultConnection")));
 });
-
 
 // Add services to the container.
 builder.Services.AddIdentity<User, IdentityRole<Guid>>(options =>
